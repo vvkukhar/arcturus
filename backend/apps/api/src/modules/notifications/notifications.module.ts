2fee8197464e32dda75f8268cd22a1d3267d3c12
Vlad <vladkuhar22082007@gmail.com>
2026-05-26 17:46:40 +0300

    fix backend build errors

--- backend/apps/api/src/modules/notifications/notifications.module.ts
+++ backend/apps/api/src/modules/notifications/notifications.module.ts
@@ -4,9 +4,11 @@ import { TelegramService } from './telegram.service';
 import { TelegramWebhookController } from './telegram-webhook.controller';
 import { TelegramWebhookService } from './telegram-webhook.service';
 import { NotificationsController } from './notifications.controller';
+import { ShippingModule } from '../shipping/shipping.module';
 
 @Global()
 @Module({
+  imports: [ShippingModule],
   controllers: [TelegramWebhookController, NotificationsController],
   providers: [NotificationsService, TelegramService, TelegramWebhookService],
   exports: [NotificationsService, TelegramService, TelegramWebhookService],
