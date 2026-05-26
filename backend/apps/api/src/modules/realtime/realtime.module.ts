d9d48fcd6900cd2e895a325b92d7d324f360b756
Vlad <vladkuhar22082007@gmail.com>
2026-05-26 17:41:06 +0300

    fix backend build errors

--- backend/apps/api/src/modules/realtime/realtime.module.ts
+++ backend/apps/api/src/modules/realtime/realtime.module.ts
@@ -1,10 +1,17 @@
 import { Global, Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { RealtimeGateway } from './realtime.gateway';
 import { PrismaModule } from '../prisma/prisma.module';
+import { QUEUE_NAMES } from '../queue/queue.constants';
 
 @Global()
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    BullModule.registerQueue({
+      name: QUEUE_NAMES.MARKET,
+    }),
+  ],
   providers: [RealtimeGateway],
   exports: [RealtimeGateway],
 })
